2b55c2751c1d4e5e15d87dff6400fed92bc9d5a0
CoderMungan <codermungan@gmail.com>
2023-08-20 13:17:38 +0300

    Swipper index.html dosyasına alında - search buttonu post ile veriyi backe yolluyor

--- tuvalprojesi/siteApp/views.py
+++ tuvalprojesi/siteApp/views.py
@@ -7,5 +7,16 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    
+
+    # Arama post geçiliyor
+    if request.method == 'POST':
+        arama = request.POST.get('search')
+        # Post geldiyse ve boş değil ise
+        if arama.__len__:
+            print('Post Çalışıyor')
+    # Buradan sql yollacağız şuan da pass!
+
+
     return render(request, 'index.html')
+
+
