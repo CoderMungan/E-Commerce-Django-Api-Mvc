639d0e20d16deae4903d9af6f01e3f78ca21cb42
CoderMungan <codermungan@gmail.com>
2023-08-31 01:19:19 +0300

    Profile sayfası js yazıldı! Formları inherit almamız gerek!

--- tuvalprojesi/siteApp/static/js/profile.js
+++ tuvalprojesi/siteApp/static/js/profile.js
@@ -1,5 +1,21 @@
 const profile = document.getElementById('profileEdit')
+const profileShow = document.getElementById('profileShow')
+const formGonderBtn = document.getElementById('formGonder')
 
-if (profile){
+if(profile){
     profile.style.display = "none";
 }
+
+const editprofile = () => {
+    profileShow.style.display = "none";
+    profile.style.display = "block";
+}
+
+const resetProfile = () => {
+    profileShow.style.display = "block";
+    profile.style.display = "none"
+}
+
+formGonderBtn.addEventListener('submit', function(){
+    resetProfile()
+})
